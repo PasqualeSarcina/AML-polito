11567de142dfb8c3948c5d323fb729a1ac393dde
Nicolò Sanna <s347700@studenti.polito.it>
2026-01-22 19:01:02 +0100

    cleaning the folder

--- TASK2_DINOv2/train.py
+++ TASK2_DINOv2/train.py
@@ -11,7 +11,7 @@ from tqdm import tqdm
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from data.task2_DINOv2_dataset import SPairDataset
 from utils.setup_data import setup_data
-from task2_dinov2.Hyperparameters.loss import InfoNCELoss
+from task2_dinov2.loss import InfoNCELoss
 
 
 data_root = setup_data() 
